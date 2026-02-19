2c488a362843516eca55d0ddea9315bc60251fc4
Vishal795-knightrider <vk3293801@gmail.com>
2026-02-19 15:58:12 +0530

    first commit

---
+++ demo2.dart
@@ -0,0 +1,11 @@
+// int main(){ 
+//   int? x=10;       //? ka matlab=values can be null, is var ko null value carry krna allowed hai
+//   print(x!);    //it checks the value is 
+//   return 0;
+// }
+
+int main(){
+String? name;
+print(name ?? "Guest");
+return 0;
+}
---
+++ hello.dart
@@ -0,0 +1,7 @@
+// // void main(){
+//   print("Hey This is Vishal");
+// }
+
+void main(List<String> args){
+  print(args);
+}
